94c94e361bc94f42b2a5b482335a58814300a272
HaiHai-17 <137904166+HaiHai-17@users.noreply.github.com>
2024-03-28 08:18:51 +0700

    28/03

--- bai17/app/src/main/java/com/example/bai17/MainActivity.java
+++ bai17/app/src/main/java/com/example/bai17/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.bai17;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -35,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
         data.add(new Name("Teo"));
         data.add(new Name("Ty"));
 
+        ArrayList<String> menucontext = new ArrayList<>();
+        menucontext.add("Sửa");
+        menucontext.add("Xoá");
+        ArrayAdapter<String> menuadapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, menucontext);
+        ListView listView = findViewById(R.id.lsView);
+        listView.setAdapter(menuadapter);
+        registerForContextMenu(listView);
+
         nhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,5 +69,46 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new NameAdapter(R.layout.name_layout, MainActivity.this, data);
         lst.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_option, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_exit) {
+            finish();
+            return true;
+        }
+        // Xử lý các mục menu khác nếu cần
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // Lấy thông tin về mục được chọn trong ListView
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int position = info.position;
+
+        switch (item.getItemId()) {
+            case R.id.:
+                Toast.makeText(this, "Item 1 selected at position: " + position, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.clear:
+                Toast.makeText(this, "Item 2 selected at position: " + position, Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
